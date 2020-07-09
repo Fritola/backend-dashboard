d434e1a6d1cb2c69c586347b72e6afb2a5e075db
Fritola <fritola23@gmail.com>
2020-07-08 21:09:36 -0300

    environment var and heroku config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+require('dotenv-safe').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
 const routes = require('./routes/index.routes')
-
 const app = express()
 
-mongoose.connect('mongodb+srv://gustavo:KfgMHE4AV07xvCP1@cluster0-ujwps.mongodb.net/qualicorp?retryWrites=true&w=majority', 
+mongoose.connect(process.env.CONNECTION_STRING, 
 {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
-    console.log("Server running at 3000")
+app.listen(process.env.PORT || 3000 , () => {
+    console.log("Server running at")
 })
 
