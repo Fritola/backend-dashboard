32b72a89d5baf2e9381b47f9e4998850966bfaf7
Fritola <fritola23@gmail.com>
2020-07-08 22:12:04 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv-safe').config()
+const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -8,10 +9,11 @@ const app = express()
 mongoose.connect(process.env.CONNECTION_STRING, 
 {useNewUrlParser: true, useUnifiedTopology: true})
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3000 , () => {
-    console.log("Server running at")
+    console.log("Server running at 3000")
 })
 
